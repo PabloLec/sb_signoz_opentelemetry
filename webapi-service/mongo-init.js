2b0660a759ce4f8d3c8881704f1d87b6f8f810ae
PabloLec <pablo.lecolinet@gmail.com>
2025-01-18 15:46:34 +0100

    Reorder docker-compose files and remove unnecessary mounts

--- webapi-service/mongo-init.js
+++ webapi-service/mongo-init.js
@@ -1,15 +1,13 @@
-db = db.getSiblingDB('webapi_db');
-
-db.createUser({
-    user: "root",
-    pwd: "password",
-    roles: [ "root" ]
+db.getSiblingDB("admin").createUser({
+    user: "signoz",
+    pwd: "signoz",
+    roles: [{ role: 'clusterMonitor', db: 'admin' }]
 })
 
-db.createUser({
+db.getSiblingDB('webapi_db').createUser({
     user: "appuser",
     pwd: "apppassword",
     roles: [{ role: "readWrite", db: "webapi_db" }]
 });
 
-db.createCollection('users');
+db.getSiblingDB('webapi_db').createCollection('users');
